e2554112d797c93aa62f6c1ca9d4f796564f6539
nycollasWildney <nycollas.wildney@gmail.com>
2025-10-23 20:16:40 -0300

    funcoes para tornar array uma mensagem e o inverso

--- PDA.h
+++ PDA.h
@@ -2,6 +2,7 @@
 #define PDA_H
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // Estrutur para definir o valor possivel de uma mensagem 
 typedef enum {
@@ -23,6 +24,43 @@ void mensagem_init(mensagem *m, tipo_mensagem t, const char *p, int fonte, int d
     m->IDdestino = destino;
     strncpy(m->payload, p, sizeof(m->payload) - 1);
     m->payload[sizeof(m->payload) - 1] = '\0'; // Garantir terminação nula
+}
+
+//funcao para tornar o array em payload para ser enviado
+void array_from_mensagem(roteador *r,mensagem *m){
+    char c[15];
+    strcpy(m->payload,"RESPOSTA:");//RESPOSTA:
+    sprintf(c,"%d:",r->id);
+    strcat(m->payload,c);//RESPOSTA:id:
+
+    for(int i=0;i< r->num_vizinhos;i++){
+        sprintf(c,"%d-%d;",r->vizinhos[i],r->custos[i]);
+        strcat(m->payload,c);
+    }
+    strcat(m->payload,"!");//indicar o fim do array
+}
+
+//funcao para tornar o payload em array
+void mensagem_from_array(roteador *r,mensagem *m,int *caminhos[][10]){
+    //"PERGUNTA: 1: 1-2;2-1;3-6;!"
+    char temp[101];
+    strcpy(temp,m->payload);
+
+    char *token = strtok(temp, ":");//token = RESPOSTA:
+    token = strtok(NULL, ":");//token = 1
+    token = strtok(NULL,"!");// token = 1-2;2-1;3-6;
+
+    char *subtoken = strtok(token,";");//subtoken = 1-2
+    while(subtoken != NULL){
+        char *id_str = strtok(subtoken,"-");// 1-2 => id_str = 1
+        char *custo_str = strtok(NULL,"-"); // 1-2 => custo_str = 2
+        if(id_str != NULL && custo_str != NULL){
+            int id = atoi(id_str);
+            int custo = atoi(custo_str);
+            caminhos[m->IDfonte][id] = custo;
+        }
+        subtoken = strtok(NULL,";");
+    }
 }
 
 #endif
--- main.c
+++ main.c
@@ -8,10 +8,16 @@
 #include <arpa/inet.h>//biblioteca para manipulação de endereços IP
 #include <sys/socket.h>//biblioteca para sockets
 #include <pthread.h>//biblioteca para threads
+#define MAX_VIZINHOS 10
 
 /*
     Essas funções são ponteiro para ser usadas na função de criar threads
     o argumento é ponteiro arg para receber o roteador ou outros argumentos
+*/
+
+/*
+    FALTA COLOCAR UMA FUNCAO PARA CALCULAR QUAL MELHOR CAMINHO ATE UM 
+    DESTINO USANDO BELLMAN-FORD E VERIFICAR SE O ARRAY ESTA CERTO
 */
 
 void *thread_enviar(void *arg){
@@ -24,15 +30,15 @@ void *thread_enviar(void *arg){
             continue;
         }
         mensagem m = fila_pop(&r->fila_saida);
-        if(m.tipo == CONTROLE){
-            continue;
-            //poor equanto não faz nada
-        }
-        else if(m.tipo == DADO){
+        // if(m.tipo == CONTROLE){
+        //     continue;
+        //     //poor equanto não faz nada
+        // }
+        // else if(m.tipo == DADO){
             enviar(r, &m);
-            printf("\033[1;32m Payload: %s\n \033[0m\n",m.payload);//printa payload em VERDE
-            continue;
-        }
+            printf("\033[1;31m Payload: %s\n \033[0m\n",m.payload);//printa payload em VERMELHO
+            // continue;
+        // }
     }
     return NULL;
 }
@@ -72,17 +78,67 @@ void *thread_processar(void *arg){
             usleep(100000); // Dorme por 100 ms
             continue;
         }
-
+        
         m = fila_pop(&r->fila_entrada);
-        //processar a mensagem recebida
-        printf("Roteador %d: Processando mensagem recebida de roteador %d\n", r->id, m.IDfonte);
-        printf("\033[1;34m Payload: %s\n \033[0m\n",m.payload);
+        if(m.tipo == CONTROLE){
+            mensagem_controle(r,&m);
+            continue;
+        }
+        if(m.tipo == DADO){
+            //processar a mensagem recebida
+            printf("Roteador %d: Processando mensagem recebida de roteador %d\n", r->id, m.IDfonte);
+            printf("\033[1;32m Payload: %s\n \033[0m\n",m.payload);
+            continue;
+        }
     }
     return NULL;
 }
 
+//usada para periodicamente manda rmensagem de controle
+void *time_controle(void *arg){
+    roteador *r = (roteador*) arg;
+
+    while(1){
+        mensagem m;
+        m.tipo = CONTROLE;
+        m.IDfonte = r->id;
+        //colocando no payload de quem é a pergutna e a resposta: mesmo que nao precise 
+        snprintf(m.payload,sizeof(m.payload),"PERGUNTA:");
+
+        for(int i=0;i < r->num_vizinhos; i++){
+            m.IDdestino = r->vizinhos[i];
+            fila_push(&r->fila_saida,m);
+            printf("\033[1;31m Payload: %s\n \033[0m\n",m.payload);
+        }
+        //dorme por 10s antes da proxima mensagem de controle
+        sleep(10);
+    }
+}
+
+//usada quando receber a mensagem de controle pede decidir se manda com o array ou manda a solicitação de array
 void mensagem_controle(roteador *r, mensagem *m){
-    
+    if(m->tipo != CONTROLE){
+        printf("\033[1;31m a mensagem recebida não é de controle \n \033[0m\n");
+        return;
+    }
+
+    if(strcmp(m->payload,"PERGUNTA:") == 0){
+        array_from_mensagem(r,m);
+        pritnf("\033[1;31m Payload %s\n \033[0m\n",m->payload);
+        return;
+    }
+    if(strncmp(m->payload,"RESPOSTA:",9) == 0){
+        int caminhos[MAX_VIZINHOS][MAX_VIZINHOS];
+        mensagem_from_array(r,m,caminhos);
+        /*
+            aqui colocar um controle de quantas mensagens de controle
+            sairam para controlar se ja recebeu todas as respostas 
+            e apos isso verificar caminhos curtos e atualiza a matriz 
+            e depois avisa as threads que a matriz mudou e manda mensagens de 
+            controle dizendo que mudou
+        */
+        return;
+    }
 }
 
 int main(int argc, char *argv[]){
@@ -98,6 +154,4 @@ int main(int argc, char *argv[]){
     cfg = ler_config(r.id); 
     roteador_init(&r, &cfg, r.id);
     printf("Roteador %d iniciado na porta %d com IP %s\n", r.id, r.porta, r.ip);
-
-
 }
